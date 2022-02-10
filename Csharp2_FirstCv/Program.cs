3e07a08f5f26a08f4911386897ad30bbd0115ac6
kub0649 <kub0649@PCFEIB405-144>
2022-02-10 11:05:23 +0100

    Add project files.

---
+++ Csharp2_FirstCv/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Csharp2_FirstCv
+{
+    delegate string Transform(string text);
+
+    delegate int Operation(int a, int b);
+
+    class Test
+    {
+        public static string ToUpper(string txt)
+        {
+            return txt.ToUpper();
+        }
+
+    }
+
+
+    class Calculator
+    {
+        private int x;
+        private int y;
+
+        public void SetXY(int x,int y)
+        {
+            this.x = x;
+            this.y = y;
+
+        }
+
+        public void Execute(Operation op)
+        {
+            int tmp = op(x, y);
+
+            Console.WriteLine(tmp);
+        }
+       
+
+
+    }
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+
+            Transform a = Test.ToUpper;
+            Console.WriteLine(a?.Invoke("jan"));
+
+            Calculator c = new Calculator();
+            c.SetXY(20, 5);
+            c.Execute((a, b) => a * b);
+
+
+
+        }
+    }
+}
